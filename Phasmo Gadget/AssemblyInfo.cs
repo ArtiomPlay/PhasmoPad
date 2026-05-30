1e2cb090fde49f63fa00ee4dc5956880e3815794
Artiom <artiomgarbul@gmail.com>
2026-05-30 19:59:51 +0300

    Changed version in field that forgot to change previously

--- Phasmo Gadget/AssemblyInfo.cs
+++ Phasmo Gadget/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("565f556b-f2c2-4485-aa12-3fc945c079f1")]
-[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyVersion("2.1.1")]
 [assembly: AssemblyFileVersion("2.1.1")]
